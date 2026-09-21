3bdd2de83fdde14bfa3011314d4c4f68227ccca9
blacksmithoffire <blacksmithoffire@gmail.com>
2026-09-21 10:32:37 +0530

    Commented out the navbar component, using temporynav for responsive use

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import Navbar from './components/Navbar'
+// import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import SelectedWork from './components/MyProjects'
 import Service from './components/Skills'
 import Experience from './components/Experience'
 import Contact from './components/Contact'
+import TemporyNav from './components/TemporyNav'
 // import './app.css'
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-paper px-4 pb-4 pt-4">
-      <Navbar />
+    <div className="min-h-screen bg-paper px-4 pb-4 pt-4 ">
+      {/* <Navbar /> */}
+      <TemporyNav/>
       <main>
         <Hero />
         <SelectedWork />
